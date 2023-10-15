3dd58c9c3651ed1d1463c361832f8a417bbb12dc
Gaspar PC <gasparrobortellamutti@gmail.com>
2023-10-15 20:04:38 -0300

    update deploy

--- tetrisProject/main.js
+++ tetrisProject/main.js
@@ -532,10 +532,10 @@ const tetrisStyles = {
   },
 };
 const tetrisSounds = {
-  gameTheme: new Audio("assets/gameTheme.mp3"),
-  gameOver: new Audio("assets/gameOver.mp3"),
-  gameError: new Audio("assets/gameError.mp3"),
-  gameScore: new Audio("assets/gameScore.mp3"),
+  gameTheme: new Audio("audio/gameTheme.mp3"),
+  gameOver: new Audio("audio/gameOver.mp3"),
+  gameError: new Audio("audio/gameError.mp3"),
+  gameScore: new Audio("audio/gameScore.mp3"),
 };
 
 const tetrisProperties = {
